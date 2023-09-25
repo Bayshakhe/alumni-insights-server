24fd1ac4d5841f52435ed7b353c0ed4477f777cc
Bayshakhe <bayshakhepramanik@gmail.com>
2023-09-25 11:51:41 +0600

    get single student and fixed some error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMET_Secret_Key);
 const app = express();
 const port = process.env.PORT || 5000;
 // const routes = require('./routes')
@@ -14,7 +15,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { default: Stripe } = require("stripe");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qlguchx.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,14 +35,46 @@ async function run() {
     // await client.connect();
 
     // get all students
-    app.get("/students", async (req, res) => {
+    app.get("/allStudents", async (req, res) => {
       const result = await studentsCollections.find().toArray();
-      // console.log(result)
+      // console.log(result);
       res.send(result);
     });
 
+    // get alumni students
+    app.get("/alumniStudents", async (req, res) => {
+      const query = { jobStatus: true };
+      const result = await studentsCollections.find(query).toArray();
+      // console.log(result);
+      res.send(result);
+    });
+
+    // get single student info
+    // app.get("/students/:id", async (req, res) => {
+    //   const id = req?.params;
+    //   // console.log(id);
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await studentsCollections.findOne(query);
+    //   // console.log(result)
+    //   res.send(result);
+    // });
+
+    app.get("/students/:id", async (req, res) => {
+      const { id } = req.params;
+
+      try {
+        const objectId = new ObjectId(id);
+        const query = { _id: objectId };
+        const result = await studentsCollections.findOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(400).send({ error: "Invalid ID format" });
+      }
+    });
+
     // post signup student data
-    app.post("/students", async (req, res) => {
+    app.post("/register", async (req, res) => {
       const postBody = req.body;
       const query = { email: postBody?.email };
       const exist = await studentsCollections.findOne(query);
@@ -66,6 +100,20 @@ async function run() {
           error: "Something wrong with your email or password",
         });
       }
+    });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "BDT",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
